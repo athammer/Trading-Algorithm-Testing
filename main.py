eadea5861de3f2c740f461fe3d6dfe01d9c39eca
athammer <athammer@buffalo.edu>
2017-07-08 00:20:00 +0000

    Adding base plate

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("4*4 !=", 2*2)
+def initialize(context):
+    # Reference to AAPL
+    context.aapl = sid(24)
+
+def handle_data(context, data):
+    # Position 100% of our portfolio to be long in AAPL
+    order_target_percent(context.aapl, 1.00)
+    data.can_trade(context.aapl)
